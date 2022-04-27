f1f13d854d64f59810e820c2ae21252ad945b807
Your Name <you@example.com>
2022-04-27 11:40:10 +0800

    Modified Teacher Class

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,9 @@
 public class Teacher {
+    String firstname,lastname,idnumber;
+    Teacher(String firstname,String lastname,String id){
+        this.firstname=firstname;
+        this.lastname=lastname;
+        this.idnumber=id;
+    }
+
 }
