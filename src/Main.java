bf12bfdb0143102fc42eb24a3dc8c1685f441304
CedricRev <cedric.revestir@g.msuiit.edu.ph>
2022-04-27 12:07:39 +0800

    Input Student and Teacher information, then print output

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Teacher t1 = new Teacher("Abunda", "Ariane Pearl", "2020-0291");
         Student s1 = new Student("Paragele", "Tracy", "2016-1123");
-        System.out.println("Student Name: " + s1.getLastName() + ", " + s1.getFirstName());
-        System.out.println("Teacher Name: " + t1.getLastName() + ", " + t1.getFirstName());
-
+        System.out.println("Student Name: " + s1.getId_number() + s1.getLastname() + ", " + s1.getFirstname());
+        System.out.println("Teacher Name: " + t1.getidnumber() + t1.getLastname() + ", " + t1.getFirstname());
 
     }
 }
